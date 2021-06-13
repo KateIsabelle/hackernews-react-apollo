b9514ea43ec545b24bd7c3bbd93d2c56a6ae60b0
Kate Davis <kate.davis887@gmail.com>
2021-06-13 17:17:31 -0400

    fix authorization bug

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 //import * as serviceWorker from './serviceWorker';
+import { setContext } from "@apollo/client/link/context";
 
 // 1
 import {
@@ -17,9 +18,19 @@ const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 });
 
+//fix authorization bug
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjQsImlhdCI6MTYyMzYxODUxN30.A-mJRyx1YjmWkNjly_pIdU5KvPJmO0-MF5vKVXDvdHQ',
+    }
+  }
+});
+
 // 3
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
