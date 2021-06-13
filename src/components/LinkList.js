232113cc68d3e13b64d22304e44b45771acf1f1e
Kate Davis <kate.davis887@gmail.com>
2021-06-13 14:16:18 -0400

    pass gql query into useQuery hook in LinkList

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -2,40 +2,31 @@ import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 import Link from './Link';
 
-const LinkList = () => {
-  const FEED_QUERY = gql`
-    {
-      feed {
+const FEED_QUERY = gql`
+  {
+    feed {
+      id
+      links {
         id
-        links {
-          id
-          createdAt
-          url
-          description
-        }
+        createdAt
+        url
+        description
       }
     }
-  `
-
-  const linksToRender = [
-    {
-      id: '1',
-      description:
-        'Prisma gives you a powerful database toolkit 😎',
-      url: 'https://prisma.io'
-    },
-    {
-      id: '2',
-      description: 'The best GraphQL client',
-      url: 'https://www.apollographql.com/docs/react/'
-    }
-  ];
+  }
+`
+const LinkList = () => {
+  const { data } = useQuery(FEED_QUERY)
 
   return (
     <div>
-      {linksToRender.map((link) => (
-        <Link key={link.id} link={link} />
-      ))}
+      { data && (
+        <>
+          {data.feed.links.map((link) => (
+            <Link key={link.id} link={link}/>
+          ))}
+        </>
+      )}
     </div>
   );
 };
