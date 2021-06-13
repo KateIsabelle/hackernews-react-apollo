50066ac486869bd78238564ee60fbafbdd110916
Kate Davis <kate.davis887@gmail.com>
2021-06-13 18:54:19 -0400

    write signup and login mutations

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,37 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router';
+import { useQuery, gql } from '@apollo/client';
+
+
+const SIGNUP_MUTATION = gql`
+  mutation SignupMutation(
+    $email: String!
+    $password: String!
+    $name: String!
+  ) {
+    signup(
+      email: $email
+      password: $password
+      name: $name
+      ) {
+        token
+      }
+  }
+`
+
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation(
+    $email: String!
+    $password: String!
+  ) {
+    login(
+      email: $email
+      password: $password
+    ) {
+      token
+    }
+  }
+`
 
 const Login = () => {
   const history = useHistory();
