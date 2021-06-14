01a322556526c337a29657ff476f7bd3d177eccc
Kate Davis <kate.davis887@gmail.com>
2021-06-13 20:44:58 -0400

    update cache for votes

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import { useMutation, gql } from '@apollo/client'
 import { AUTH_TOKEN } from '../constants';
 import { timeDifferenceForDate } from '../utils'
 import { FEED_QUERY } from './LinkList';
-import { useMutation, gql } from '@apollo/client'
 
 const VOTE_MUTATION = gql`
   mutation VoteMutation($linkId: ID!) {
@@ -30,6 +30,28 @@ const Link = (props) => {
   const [vote] = useMutation(VOTE_MUTATION, {
     variables: {
       linkId: link.id
+    },
+    update(cache, { data: { vote }}) {
+      const { feed } = cache.readQuery({
+        query: FEED_QUERY
+      });
+      const updatedLinks = feed.links.map(feedLink => {
+        if (feedLink.id === link.id) {
+          return {
+            ...feedLink,
+            votes: [...feedLink.votes, vote]
+          }
+        }
+        return feedLink;
+      });
+      cache.writeQuery({
+        query: FEED_QUERY,
+        data: {
+          feed: {
+            links: updatedLinks
+          }
+        }
+      })
     }
   })
 
