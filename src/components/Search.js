ec0ff416fe7c62ab691735ab8867ecd070ee5589
Kate Davis <kate.davis887@gmail.com>
2021-06-13 21:45:43 -0400

    useLazyQuery hook for search button onClick execute query

--- src/components/Search.js
+++ src/components/Search.js
@@ -29,6 +29,10 @@ const FEED_SEARCH_QUERY = gql`
 
 const Search = () => {
   const [searchFilter, setSearchFilter] = useState('');
+  const [executeSearch, { data }] = useLazyQuery(
+    FEED_SEARCH_QUERY
+  );
+
   return (
     <>
       <div>
@@ -37,7 +41,15 @@ const Search = () => {
           type="text"
           onChange={(e) => setSearchFilter(e.target.value)}
         />
-        <button>OK</button>
+        <button
+          onClick={() => {
+            executeSearch({
+              variables: { filter: searchFilter }
+            })
+          }} 
+        >
+          OK
+        </button>
       </div>
       {data &&
         data.feed.links.map((link, index) => (
